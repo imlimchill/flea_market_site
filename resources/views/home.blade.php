b7d787030e640f33ed13d5db389c0bbea852460e
imlimchill <cojwd300@gmail.com>
2021-02-07 18:31:21 +0900

    デザインの変更
     QnAページのデザインの変更
     必要ないfileの削除
     細かいデザインの変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="container mx-auto px-4 pt-16">
+    <div class="container mx-auto px-4 pt-16 h-screen">
         <div class="popular-movies">
             <div class="grid grid-cols-12">
                 <h2 class="uppercase tracking-wider text-orange-700 text-lg font-semibold">ALL</h2>
--- resources/views/qna/create.blade.php
+++ resources/views/qna/create.blade.php
@@ -2,22 +2,30 @@
 
 @section('content')
 <div class="h-screen px-64 mt-5">
-    <h1 class="text-3xl font-bold">QnA Create</h1>
-    <form action="/qna" method="POST">
-        @csrf
-        <label for="title">Title</label><br>
-        <input class="w-full border-2 border-blue-500 @error('title') border-4 border-pink-500 @enderror" type="text" name="title" id="title" value="{{ old('title') }}"><br>
-        @error('title')
-            <small class="text-red-500">{{ $message }}</small><br>
-        @enderror
+    <div class="border-b-4 border-gray-800 mt-12">
+        <h1 class="text-3xl font-bold">QnA 登録</h1>
+    </div>
+    <div class="p-5">
+        <form action="/qna" method="POST">
+            <div class="mb-4 mt-4">
+            @csrf
+            <label class="text-xl" for="title">Qna Title</label><br>
+            <input class="w-full mt-1 px-3 py-1 border-2 border-gray-500 hover:border-gray-700 @error('title') border-4 border-red-500 @enderror" type="text" name="title" id="title" value="{{ old('title') }}"><br>
+            @error('title')
+                <small class="text-red-500">{{ $message }}</small><br>
+            @enderror
+            </div>
 
-        <label for="body">body</label><br>
-        <textarea class="w-full border-2 border-blue-500 @error('body') border-4 border-pink-500 @enderror" name="body" id="body" cols="30" rows="10">{{ old('body') }}</textarea>
-        @error('body')
-            <small class="text-red-500">{{ $message }}</small>
-        @enderror
-        <button class="bg-blue-400 px-4 py-2 hover:bg-blue-600 float-right text-white mb-2">Submit</button>
-    </form>
+            <label class="text-xl" for="body">Qna 内容</label><br>
+            <textarea class="w-full mt-1 px-3 py-2 border-2 border-gray-500 hover:border-gray-700 @error('body') border-4 border-red-500 @enderror" name="body" id="body" cols="30" rows="10">{{ old('body') }}</textarea>
+            @error('body')
+                <small class="text-red-500">{{ $message }}</small>
+            @enderror
+            <div class="grid justify-items-center">
+                <button class="mt-5 bg-gray-700 hover:bg-gray-900 text-white font-bold py-2 px-4 border border-gray-900">Submit</button>
+            </div>
+        </form>
+    </div>
 
 </div>
 @endsection
