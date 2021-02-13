8695e30a1696a41a30ad5d808975ae1da5febd0a
jongin <bill1224@naver.com>
2021-02-14 02:20:32 +0900

    02/14 product and member page refectoring

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,12 +17,12 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::latest()->get();
-        $photos = Photo::latest()->get();
-
+        $products = Product::OrderBy('created_at', 'desc') -> paginate(8);
+        $Categories = Categories::get();
+        
         return view('products.index', [
-            'products' => $products,
-            'photos' => $photos        
+            'products' => $products,            
+            'Categories' => $Categories     
         ]);
     }
 
@@ -39,14 +39,14 @@ class ProductController extends Controller
     {        
         request()->validate([            
             'pro_state' => 'required', 
-            'pro_price' => 'required',
+            'pro_price' => 'required|integer',
             'pro_title' => 'required', 
             'pro_explan' => 'required',
             'pro_tag' => 'exists:categories,category'
         ]);
 
         $values = request(['pro_tag', 'pro_state', 'pro_price','pro_title', 'pro_explan']);
-        $values['user_seq'] = auth() -> id();
+        $values['user_id'] = auth() -> id();
         $products = Product::create($values);
 
         if($request->has(['photo']))
@@ -56,8 +56,8 @@ class ProductController extends Controller
                 'url' => Storage::url($path),
                 'hashname' => $request->file('photo')->hashName(),
                 'originalname' => $request->file('photo')->getClientOriginalName(),
-                'user_seq' => auth()->id(),
-                'pro_seq' => $products->id          
+                'user_id' => auth()->id(),
+                'product_id' => $products->id          
                 ]);
             }
 
@@ -68,8 +68,8 @@ class ProductController extends Controller
 
     public function show(Product $product)
     {
-        $user = User::find($product->user_seq);
-        $photos = Photo::where('pro_seq', $product->id)->latest()->first();
+        $user = User::find($product->user_id);
+        $photos = Photo::where('product_id', $product->id)->latest()->first();
 
         return view('products/show' , [
             'product' => $product,
@@ -92,7 +92,7 @@ class ProductController extends Controller
     {
  
         $values = request(['pro_tag', 'pro_state', 'pro_price','pro_title', 'pro_explan']);
-        $values['user_seq'] = auth() -> id();
+        $values['user_id'] = auth() -> id();
         $product -> update($values);
 
         if($request->has(['photo']))
@@ -102,8 +102,8 @@ class ProductController extends Controller
             'url' => Storage::url($path),
             'hashname' => $request->file('photo')->hashName(),
             'originalname' => $request->file('photo')->getClientOriginalName(),
-            'user_seq' => auth()->id(),
-            'pro_seq' => $product->id          
+            'user_id' => auth()->id(),
+            'product_id' => $product->id          
         ]);
         }
 
@@ -111,12 +111,14 @@ class ProductController extends Controller
     }
 
     public function destroy(Product $product)
-    {
-        // $photos = Photo::where('pro_seq', $product->id)->get();
-        // $photos -> delete();
+    {        
         $product -> delete();
         
-        return redirect('/products');
-        // return redirect()->back();
+        return redirect('/products');        
+    }
+
+    public function filter($category)
+    {
+        return view('products.index');
     }
 }
