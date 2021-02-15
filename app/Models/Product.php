b008be21a8e008f292f3e141a6129d5d619044ba
kanghr1685 <kanghr1685@naver.com>
2021-02-15 15:39:47 +0900

    마이페이지 수정 및 구매기능

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,5 +9,17 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['pro_tag', 'pro_state', 'pro_price', 'pro_title', 'pro_explan', 'user_seq'];
+    protected $fillable = ['pro_tag', 'pro_state', 'pro_price', 'pro_title', 'pro_explan', 'user_id'];
+
+    public function photo()
+    {
+        return $this->hasOne(Photo::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
 }
+
